b0fe9ba618387fbc9f5268dcbe80b582f009291f
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-06-06 10:14:36 +0100

    Create Inheritance.kt

---
+++ Inheritance.kt
@@ -0,0 +1,88 @@
+fun main() {
+
+    val dog = Dog("Efes")
+    val cat = Cat("Maya")
+    val pokemon = Pokemon("Mew")
+    
+    dog.eat()
+    dog.printEnergy()
+    cat.updateColor("gray")
+    println(pokemon.claws)
+    println(cat.claws)
+
+}
+open class Animal(val name:String){
+
+    protected var energy = 0
+    open var color = "Black"
+
+    init {
+        println("Animal class initializing...")
+    }
+
+    fun printEnergy(){
+        println("Current $name energy : $energy")
+    }
+    open fun eat(){
+        energy += 1
+    }
+    open fun run(){
+        energy -= 1
+    }
+}
+
+class Dog(dogName : String): Animal(dogName){
+    init {
+        println("Dog class initializing...")
+    }
+
+    override fun eat() {
+        energy +=5
+    }
+
+    override fun run() {
+        energy -=5
+    }
+}
+
+open class Cat(dogName : String): Animal(dogName){
+    init {
+        println("Cat class initializing...")
+    }
+
+    open var claws = "Sharp"
+
+    override fun eat() {
+        energy +=10
+    }
+
+    override fun run() {
+        energy -=10
+    }
+}
+
+
+class Pokemon(pokemonName : String): Cat(pokemonName){
+    init {
+        println("Pokemon class initializing...")
+    }
+
+    override var claws = "Smooth"
+
+    override fun eat() {
+        energy +=8
+    }
+
+    override fun run() {
+        energy -=8
+    }
+}
+
+fun Animal.updateColor(color: String) {
+    this.color = color
+
+}
+
+
+//open class Parent
+//class Child:Parent()
