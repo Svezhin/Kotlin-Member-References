bb1bb50d2d9be752f746b36d3d8a822ca788c9e0
Svezhin <134381910+Svezhin@users.noreply.github.com>
2023-06-06 09:41:34 +0100

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,52 @@
+fun main(args: Array<String>) {
+
+    val personList = listOf(
+        Person("Ben", 37, true),
+        Person("Martin", 5, true),
+        Person("Haggard", 39, false),
+        Person("Angel", 25, false),
+        Person("David", 66, true)
+    )
+//
+//    personList.filter {
+//        it.male
+//    }.forEach{
+//        println(it.name)
+//    }
+//
+//    personList.filter {
+//        !it.male
+//    }.forEach{
+//        println(it.name)
+//    }
+//
+
+//    personList.filter(Person::male).forEach { println(it.name) } // Filter just male
+//    personList.sortedBy(Person::name).forEach { println(it.name) } // Sort names as Alphabetical
+//    personList.sortedByDescending(Person::name).forEach { println(it.name) } // Sort names as Alphabetical Reversed
+//
+//    println(personList.any(Person::important))
+//    println(personList.any(::important2))
+
+    val names = listOf("dog","cat")
+    println(names.mapIndexed(::Animal))
+    println(names.mapIndexed{ id, name ->
+        Animal(5,"cobra")
+    })
+
+}
+fun Person.important() = this.name.startsWith(prefix = "M") && this.age > 20
+
+fun important2(person: Person) =
+    person.name.startsWith("S") && person.age > 18
+
+
+data class Person(
+    val name: String,
+    val age: Int,
+    val male: Boolean)
+
+data class Animal(
+    val id: Int,
+    val name: String
+)
